0411e5b158ce460e9e966106318e93ebd60799e3
flatwhite <flatwhite@flatwhiteui-MacBookAir.local>
2023-07-16 20:00:03 +0900

    h2 version r2dbc tutorial

--- src/main/java/com/tutorial/webflux/r2dbc/R2dbcWebfluxApplication.java
+++ src/main/java/com/tutorial/webflux/r2dbc/R2dbcWebfluxApplication.java
@@ -2,7 +2,9 @@ package com.tutorial.webflux.r2dbc;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.data.r2dbc.repository.config.EnableR2dbcRepositories;
 
+@EnableR2dbcRepositories
 @SpringBootApplication
 public class R2dbcWebfluxApplication {
 
---
+++ src/main/java/com/tutorial/webflux/r2dbc/infrastructure/H2ServerConfig.java
@@ -0,0 +1,29 @@
+package com.tutorial.webflux.r2dbc.infrastructure;
+
+
+import lombok.extern.slf4j.Slf4j;
+import org.h2.tools.Server;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.context.event.ContextClosedEvent;
+import org.springframework.context.event.ContextRefreshedEvent;
+import org.springframework.context.event.EventListener;
+import org.springframework.stereotype.Component;
+
+@Slf4j
+@Component
+public class H2ServerConfig {
+    @Value("${h2.console.port}")
+    private String port;
+    private Server webServer;
+
+    @EventListener(ContextRefreshedEvent.class)
+    public void start() throws java.sql.SQLException {
+        log.info("started h2 console at port {}.", port);
+        this.webServer = Server.createWebServer("-webPort", port).start();
+    }
+
+    @EventListener(ContextClosedEvent.class)
+    public void stop() {
+        log.info("stopped h2 console at port {}.", port); this.webServer.stop();
+    }
+}
